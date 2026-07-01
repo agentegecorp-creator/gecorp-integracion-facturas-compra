fd1674e2652cd95c93b29b20ffe27dc4687a117d
Agente Gecorp <agentegecorp@Air-de-Agente.NC-WR644GACV>
2026-07-01 18:15:49 -0400

    Expand OC-managed vendor list

--- src/lib/review/oc-managed-vendors.ts
+++ src/lib/review/oc-managed-vendors.ts
@@ -10,6 +10,10 @@ export const OC_MANAGED_VENDORS: OcManagedVendor[] = [
   { rut: '78425850-5', normalizedRut: '784258505', name: 'BALL CHILE S.A.' },
   { rut: '91942000-6', normalizedRut: '919420006', name: 'MALTEXCO S.A.' },
   { rut: '87690900-6', normalizedRut: '876909006', name: 'DISTRIBUIDORA PORTLAND S.A.' },
+  { rut: '76151639-6', normalizedRut: '761516396', name: 'Sociedad Comercial Yerival Ltda.' },
+  { rut: '76376519-9', normalizedRut: '763765199', name: 'TIEMPOCAR SPA' },
+  { rut: '77140079-5', normalizedRut: '771400795', name: 'GROUP LAHH SER SPA' },
+  { rut: '76164832-2', normalizedRut: '761648322', name: 'REFRIGERACION Y ELECTRICIDAD INDUSTRIAL ALTO FRIO LIMITADA' },
 ];
 
 export const OC_MANAGED_VENDOR_RUTS = OC_MANAGED_VENDORS.map((vendor) => vendor.normalizedRut);
